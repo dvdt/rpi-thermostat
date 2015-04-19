3486577ba754067f7d6bedb04825f7eccfc78428
David Tsao <davidtsao@gmail.com>
2015-04-18 22:24:17 -0500

    temp logging fixes

--- README.md
+++ README.md
@@ -4,11 +4,17 @@ Install
 ---
 
 ```
+sudo apt-get install python-pip
+sudo apt-get install supervisor
+sudo pip install virtualenv
 virtualenv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
 sudo cp rpi-thermostat.conf /etc/supervisor/conf.d/
+sudo cp temp_logger.conf /etc/supervisor/conf.d/
 ```
+
+also make sure to install [DHT22 software from adafruit](https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging/overview)
 
 Deploy
 ---
@@ -16,7 +22,8 @@ Deploy
 ./deploy
 # in rpi
 sudo supervisorctl reread
-sudo supervisorctl update
+sudo supervisorctl restart thermostat
+sudo supervisorctl restart temp_logger
 
 # should be running now!
 ```
--- temp_logger.py
+++ temp_logger.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python
 
-import Adafruit_DHT
+try:
+    import Adafruit_DHT
+except ImportError, e:
+    class Adafruit_DHTMOCK():
+        def read_retry(self):
+            return 25, 50
+    Adafruit_DHT = Adafruit_DHTMOCK()
 import requests
 import logging
-import json
+from apscheduler.schedulers.background import BlockingScheduler
+
 THERMOSTAT_URI = 'http://192.168.1.214:5000/api/v1/temperature/'
 
 def main():
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, '18')
     if humidity is not None and temperature is not None:
         requests.post(THERMOSTAT_URI, data=dict(temperature=temperature, humidity=humidity))
-        logger.info('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        logger.warn('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
         logger.error('Failed to get reading. Try again!')
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(asctime)s %(message)s')
+    logging.basicConfig(level=logging.WARN, format='%(levelname)s - %(asctime)s %(message)s')
     logger = logging.getLogger('main')
-    main()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(main, 'interval', seconds=60)
+    logger.warn('starting scheduler')
+    scheduler.start()
+
